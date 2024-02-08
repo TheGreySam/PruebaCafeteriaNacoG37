2cd340db09df395be1aee8673430cdee3b833ed4
Sebastián Gómez Letelier <53100726+TheGreySam@users.noreply.github.com>
2024-02-07 23:07:54 -0300

    ruta get testeada

--- tests/server.spec.js
+++ tests/server.spec.js
@@ -1,6 +1,15 @@
 const request = require("supertest");
 const server = require("../index");
 
+const cafes = require("../cafes.json")
+
 describe("Operaciones CRUD de cafes", () => {
+    // GET /cafes
+  it("responds with array", async () => {
+    const response = await request(server).get('/cafes')
+     
+    expect(response.status).toBe(200);
+    expect(Array.isArray(response.body)).toBe(true)
+  });
 
 });
